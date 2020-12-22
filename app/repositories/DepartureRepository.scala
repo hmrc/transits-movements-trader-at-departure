b7d34be434f321203329e08132b723f9cf74faac
MatthewAlexanderBeck <MatthewAlexanderBeck@users.noreply.github.com>
2020-12-22 08:49:02 +0000

    CTCTRADERS-1615: Added lrn indexing

--- app/repositories/DepartureRepository.scala
+++ app/repositories/DepartureRepository.scala
@@ -40,17 +40,20 @@ import scala.util.Try
 
 class DepartureRepository @Inject()(mongo: ReactiveMongoApi, appConfig: AppConfig)(implicit ec: ExecutionContext) extends MongoDateTimeFormats {
 
-  private val index: Aux[BSONSerializationPack.type] = IndexUtils.index(
+  private val eoriNumberIndex: Aux[BSONSerializationPack.type] = IndexUtils.index(
     key = Seq("eoriNumber" -> IndexType.Ascending),
     name = Some("eori-number-index")
   )
 
-  private val cacheTtl = appConfig.cacheTtl
+  private val referenceNumberIndex : Aux[BSONSerializationPack.type] = IndexUtils.index(
+    key = Seq("referenceNumber" -> IndexType.Ascending),
+    name = Some("reference-number-index")
+  )
 
   private val lastUpdatedIndex: Aux[BSONSerializationPack.type] = IndexUtils.index(
     key = Seq("lastUpdated" -> IndexType.Ascending),
     name = Some("last-updated-index"),
-    options = BSONDocument("expireAfterSeconds" -> cacheTtl)
+    options = BSONDocument("expireAfterSeconds" -> appConfig.cacheTtl)
   )
 
   val started: Future[Unit] = {
@@ -58,7 +61,8 @@ class DepartureRepository @Inject()(mongo: ReactiveMongoApi, appConfig: AppConfi
       .flatMap {
         jsonCollection =>
           for {
-            _   <- jsonCollection.indexesManager.ensure(index)
+            _   <- jsonCollection.indexesManager.ensure(eoriNumberIndex)
+            _   <- jsonCollection.indexesManager.ensure(referenceNumberIndex)
             res <- jsonCollection.indexesManager.ensure(lastUpdatedIndex)
           } yield res
       }
