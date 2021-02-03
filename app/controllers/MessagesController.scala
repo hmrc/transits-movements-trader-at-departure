fcdb52633d5a12fa2e8f26ddbf04938a9f5f9af3
Robert Laverick <rob.laverick@digital.hmrc.gov.uk>
2021-02-03 15:23:10 +0000

    2011 update

--- app/controllers/MessagesController.scala
+++ app/controllers/MessagesController.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 HM Revenue & Customs
+ * Copyright 2021 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -60,7 +60,7 @@ class MessagesController @Inject()(
               request.departure.status.transition(MessageReceivedEvent.DeclarationCancellationRequest) match {
                 case Right(status) =>
                   submitMessageService
-                    .submitMessage(departureId, request.departure.nextMessageId, message, status)
+                    .submitMessage(departureId, request.departure.nextMessageId, message, status, request.channel)
                     .map {
                       case SubmissionProcessingResult.SubmissionFailureInternal =>
                         InternalServerError
