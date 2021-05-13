fc7f06721e06d1cc5eed1be303a4eca1eda8946a
Robert Laverick <rob.laverick@digital.hmrc.gov.uk>
2021-05-13 17:14:54 +0100

    removed badrequest route on missing clientId to ensure API won't break front-end

--- app/controllers/actions/AuthenticatedClientIdAction.scala
+++ app/controllers/actions/AuthenticatedClientIdAction.scala
@@ -16,31 +16,27 @@
 
 package controllers.actions
 
-import config.AppConfig
 import models.request.AuthenticatedClientRequest
 import models.request.AuthenticatedRequest
-import play.api.mvc.Results.BadRequest
-import play.api.mvc.ActionRefiner
-import play.api.mvc.Result
+import play.api.mvc.ActionTransformer
 import uk.gov.hmrc.auth.core.AuthConnector
 import uk.gov.hmrc.auth.core.AuthorisedFunctions
 import uk.gov.hmrc.auth.core.retrieve.v2.Retrievals
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.HeaderCarrierConverter
-
 import javax.inject.Inject
+
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
 
 class AuthenticatedClientIdAction @Inject()(override val authConnector: AuthConnector)(implicit val executionContext: ExecutionContext)
-    extends ActionRefiner[AuthenticatedRequest, AuthenticatedClientRequest]
+    extends ActionTransformer[AuthenticatedRequest, AuthenticatedClientRequest]
     with AuthorisedFunctions {
-  override protected def refine[A](request: AuthenticatedRequest[A]): Future[Either[Result, AuthenticatedClientRequest[A]]] = {
+
+  override protected def transform[A](request: AuthenticatedRequest[A]): Future[AuthenticatedClientRequest[A]] = {
     implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromHeadersAndSession(request.headers)
     authorised()
-      .retrieve(Retrievals.clientId) {
-        case Some(clientId) => Future.successful(Right(AuthenticatedClientRequest(request, request.channel, request.eoriNumber, clientId)))
-        case None           => Future.successful(Left(BadRequest("Unable to retrieve clientId")))
-      }
+      .retrieve(Retrievals.clientId)(clientOpt => Future.successful(AuthenticatedClientRequest(request, request.channel, request.eoriNumber, clientOpt)))
   }
+
 }
