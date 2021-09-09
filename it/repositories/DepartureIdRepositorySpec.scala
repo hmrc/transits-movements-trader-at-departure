61fd271e49d56c9869d6c54d53265aa0bda3123a
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2021-09-09 17:12:29 +0100

    remove-warnings: Integration test warnings removed.

--- it/repositories/DepartureIdRepositorySpec.scala
+++ it/repositories/DepartureIdRepositorySpec.scala
@@ -17,13 +17,12 @@
 package repositories
 
 import models.DepartureId
-import org.scalatest.concurrent.IntegrationPatience
-import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.libs.json.Json
-import reactivemongo.play.json.ImplicitBSONHandlers.JsObjectDocumentWriter
+import reactivemongo.play.json.collection.Helpers.idWrites
 import reactivemongo.play.json.collection.JSONCollection
 
 import scala.concurrent.ExecutionContext.Implicits.global
