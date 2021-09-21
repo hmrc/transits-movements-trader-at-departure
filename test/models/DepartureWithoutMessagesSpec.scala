410ba86cda40287bf956c81197bee075849e8159
MatthewAlexanderBeck <MatthewAlexanderBeck@users.noreply.github.com>
2021-09-21 16:13:39 +0100

    CTCTRADERS-2636: model tests

--- test/models/DepartureWithoutMessagesSpec.scala
+++ test/models/DepartureWithoutMessagesSpec.scala
@@ -19,36 +19,94 @@ package models
 import base.SpecBase
 import cats.data.NonEmptyList
 import generators.ModelGenerators
-import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks.forAll
+import models.MessageType.DeclarationRejected
+import models.MessageType.DepartureDeclaration
+import models.MessageType.MrnAllocated
+import models.MessageType.PositiveAcknowledgement
 import org.scalacheck.Arbitrary.arbitrary
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks.forAll
+import play.api.libs.json.JsObject
 import play.api.libs.json.Json
 
 import java.time.LocalDateTime
 
-class DepartureWithoutMessagesSpec extends SpecBase with ModelGenerators {
+class DepartureWithoutMessagesSpec extends SpecBase with ModelGenerators with MongoDateTimeFormats {
 
   "DepartureWithoutMessages" - {
+    "apply" - {
+      "must return latest message type" in {
+
+        forAll(arbitrary[Departure], arbitrary[MessageWithStatus]) {
+          (departure, message) =>
+            val expectedDateTime             = LocalDateTime.now
+            val expectedDateTimeMinusMinutes = LocalDateTime.now.minusMinutes(10)
+            val expectedDateTimeMinusHours   = LocalDateTime.now.minusHours(2)
+            val expectedDateTimeMinusDays    = LocalDateTime.now.minusDays(1)
+
+            val message1        = message.copy(dateTime = expectedDateTimeMinusDays, messageType = PositiveAcknowledgement)
+            val message2        = message.copy(dateTime = expectedDateTimeMinusMinutes, messageType = DepartureDeclaration)
+            val message3        = message.copy(dateTime = expectedDateTimeMinusHours, messageType = MrnAllocated)
+            val expectedMessage = message.copy(dateTime = expectedDateTime, messageType = DeclarationRejected)
+
+            val departureWithDateTime = departure.copy(
+              messages = NonEmptyList(message1, List(message2, message3, expectedMessage))
+            )
+
+            val result = DepartureWithoutMessages.fromDeparture(departureWithDateTime)
+
+            result.latestMessage mustBe DeclarationRejected
+        }
+      }
+    }
+
     "must Serialise and return latest message type" in {
 
-      forAll(arbitrary[Departure], arbitrary[MessageWithStatus]) {
-        (x, y) =>
-          val expectedDateTime             = LocalDateTime.now
-          val expectedDateTimeMinusMinutes = LocalDateTime.now.minusMinutes(10)
-          val expectedDateTimeMinusHours   = LocalDateTime.now.minusHours(2)
-          val expectedDateTimeMinusDays    = LocalDateTime.now.minusDays(1)
+      forAll(arbitrary[Departure]) {
+        departure =>
+          val json = Json.obj(
+            "_id"                      -> departure.departureId,
+            "channel"                  -> departure.channel,
+            "eoriNumber"               -> departure.eoriNumber,
+            "movementReferenceNumber"  -> departure.movementReferenceNumber,
+            "referenceNumber"          -> departure.referenceNumber,
+            "status"                   -> departure.status,
+            "created"                  -> departure.created,
+            "lastUpdated"              -> departure.lastUpdated,
+            "notificationBox"          -> departure.notificationBox,
+            "nextMessageId"            -> departure.nextMessageId,
+            "nextMessageCorrelationId" -> departure.nextMessageCorrelationId,
+            "messages" -> Json.arr(
+              Json.obj(
+                "messageId"            -> 1,
+                "dateTime"             -> LocalDateTime.now.minusHours(2),
+                "messageType"          -> "IE928",
+                "message"              -> "<foo></foo>",
+                "messageCorrelationId" -> 1,
+                "messageJson"          -> ""
+              ),
+              Json.obj(
+                "messageId"            -> 2,
+                "dateTime"             -> LocalDateTime.now,
+                "messageType"          -> "IE015",
+                "message"              -> "<foo></foo>",
+                "messageCorrelationId" -> 1,
+                "messageJson"          -> ""
+              ),
+              Json.obj(
+                "messageId"            -> 3,
+                "dateTime"             -> LocalDateTime.now.minusDays(5),
+                "messageType"          -> "IE016",
+                "message"              -> "<foo></foo>",
+                "messageCorrelationId" -> 1,
+                "messageJson"          -> ""
+              )
+            )
+          )
 
-          val message1        = y.copy(dateTime = expectedDateTimeMinusDays)
-          val message2        = y.copy(dateTime = expectedDateTimeMinusMinutes)
-          val message3        = y.copy(dateTime = expectedDateTimeMinusHours)
-          val expectedMessage = y.copy(dateTime = expectedDateTime)
+          val result = json.validate[DepartureWithoutMessages].asOpt.value
 
-          val departure = x.copy(messages = NonEmptyList(message1, List(message2, message3, expectedMessage)))
-
-          val departureWithoutMessages: DepartureWithoutMessages = DepartureWithoutMessages.fromDeparture(departure)
-
-          Json.toJsObject(departureWithoutMessages).validate[Departure] mustBe expectedMessage.messageId
+          result.latestMessage mustBe DepartureDeclaration
       }
     }
   }
-
 }
